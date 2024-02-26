e985b3dcfb4b4552d2383feb338c6bd68092cfa8
Chiou Jia Sheng <100426464+Chious@users.noreply.github.com>
2024-02-26 20:46:19 +0800

    build: MVC

---
+++ README.md
@@ -0,0 +1,56 @@
+# Alpha Camp Dev C3 M3 - Restaurant List
+
+This is a first update solution Dev C3 M3
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- RWD by flex setting
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [https://github.com/Chious/Dev-C4-M1-Final](https://github.com/Chious/Dev-C4-M1-Final)
+- Live Site URL: [https://chious.github.io/Dev-C4-M1-Final/](https://chious.github.io/Dev-C4-M1-Final/)
+
+## My process
+
+### Built with
+
+- [Express.js](https://expressjs.com) -- backend framework
+- [handlebar.js](https://handlebarsjs.com) -- view template to host html without webpack
+- [Boostrap](https://getbootstrap.com) -- UI Library
+
+### 處理步驟
+
+### What I learned
+
+### 待迭代
+
+### Useful resources
+
+## Author
+
+- Github - [邱佳昇](https://github.com/Chious)
+
+## Acknowledgments None
--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const movies = require("./public/data/movies.json").results;
+const BASE_IMG_URL = "https://movie-list.alphacamp.io/posters/";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const restaurants = require("./public/data/restaurant.json").results;
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+app.get("/movies", (req, res) => {
+  const keyword = req.query.search?.trim();
+  const matchedMovies = keyword
+    ? movies.filter((mv) =>
+        Object.values(mv).some((property) => {
+          if (typeof property === "string") {
+            return property.toLowerCase().includes(keyword.toLowerCase());
+          }
+          return false;
+        })
+      )
+    : movies;
+  res.render("index", { movies: matchedMovies, BASE_IMG_URL, keyword });
+});
+
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  const restaurant = restaurants.find((mv) => mv.id.toString() === id);
+  res.render("detail", { restaurant, BASE_IMG_URL });
 });
 
 app.listen(port, () => {
